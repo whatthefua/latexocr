5e5ee54a10f66261c21b7770831f09ce2c693b89
Teerapat <mek.1803@gmail.com>
2014-06-26 07:11:21 +0700

    solve for initialise large array in function more than stack

--- sobel.hpp
+++ sobel.hpp
@@ -1,18 +1,35 @@
+/*
+invert color
+*/
 #ifndef sobel
     #define sobel
 
     #include "image_object.hpp"
+    #include <cassert>
 
     void preprocessing_sobel(gy_image_object &img)
     {
-        int Gx[img.width() - 2][img.height() - 2],Gy[img.width() - 2][img.height() - 2],IM[img.width()][img.height()],i,j,r,w,h;
+        assert(img.width() > 2);
+        assert(img.height() > 2);
+        
+        int **Gx = new int*[img.width() - 2];
+        int **Gy = new int*[img.width() - 2];
+        for(size_t i=0;i<img.width()-2;i++){
+            Gx[i] = new int[img.height() - 2];
+            Gy[i] = new int[img.height() - 2];
+        }
+        
+        int **IM = new int*[img.width()];
+        for(size_t i=0;i<img.width();i++) IM[i] = new int[img.height()];
+
+        int r=0,w=0,h=0;
 
         w = img.width();
         h = img.height();
-
-        for(i = 0; i < w; i++)
+        
+        for(int i = 0; i < w; i++)
         {
-            for(j = 0; j < h; j++)
+            for(int j = 0; j < h; j++)
             {
                 IM[i][j] = img.get_pixel(i,j);
             }
@@ -23,9 +40,9 @@
 
         img.resize(w,h);
 
-        for(i = 0; i < w; i++)
+        for(int i = 0; i < w; i++)
         {
-            for(j = 0; j < h; j++)
+            for(int j = 0; j < h; j++)
             {
                 Gx[i][j] = -IM[i][j] + IM[i + 2][j] - (IM[i][j + 1] << 1) + (IM[i + 2][j + 1] << 1) - IM[i][j + 2] + IM[i + 2][j + 2];
                 Gy[i][j] = IM[i][j] - IM[i][j + 2] + (IM[i + 1][j] << 1) - (IM[i + 1][j + 2] << 1) + IM[i + 2][j] - IM[i + 2][j + 2];
@@ -33,5 +50,15 @@
                 img.set_pixel(i,j,r);
             }
         }
+        
+        for(size_t i=img.width();i>0;) delete[] IM[--i];
+        delete [] IM;
+        
+        for(size_t i=img.width()-2;i>0;){
+            delete[] Gx[--i];
+            delete[] Gy[i];
+        }
+        delete [] Gx;
+        delete [] Gy;
     }
 #endif
